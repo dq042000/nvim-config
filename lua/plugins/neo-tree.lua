5fbf935c343bc22635d771f75423584888ef20a2
祁威德 <dq042000@gmail.com>
2026-07-14 19:41:42 +0800

    fix: 更新 lazy-lock.json 中的插件版本號；修正 lazyvim.json 中的新聞檔案大小

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 -- 釋放 Neo-tree 內的 Ctrl+B（原為 scroll_preview），讓全域的視窗滿版切換也能在檔案樹裡用
 return {
-  "nvim-neotree/neo-tree.nvim",
+  "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
       mappings = {
