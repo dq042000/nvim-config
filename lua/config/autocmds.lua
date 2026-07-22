4af891627740a10a9f68ed781919e635b3efa800
祁威德 <dq042000@gmail.com>
2026-07-22 16:15:13 +0800

    feat: 於 markdown/text/gitcommit 關閉拼字檢查避免中文標紅

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- 在 markdown 等以中文為主的檔案關閉拼字檢查（避免中文字被標紅色波浪線）
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", "gitcommit" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
