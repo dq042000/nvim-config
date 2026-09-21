6af5e561369f9f334f40415b02153f98a19d7587
祁威德 <dq042000@gmail.com>
2026-09-21 15:30:40 +0800

    feat(options): 關閉存檔時自動格式化

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,8 @@ local opt = vim.opt
 
 vim.g.lazyvim_php_lsp = "intelephense" -- PHP LSP 使用 intelephense（預設為 phpactor）
 
+vim.g.autoformat = false -- 存檔時不自動格式化（需要時按 <leader>cf 手動格式化）
+
 vim.opt.clipboard = "unnamedplus" -- 讓 Vim 使用系統剪貼簿（與系統剪貼簿同步）
 
 -- 系統剪貼簿需要外部工具，換機器時提醒安裝
